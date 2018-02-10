ae55c78ece93059b5319e4c3aea70eca1a002d3e
Pushkar Kadam <pushkar.kadam@paydock.com>
2018-02-11 04:05:36 +1100

    indentations corrected

--- got_power.py
+++ got_power.py
@@ -2,20 +2,20 @@ from populate import Humanoids
 from magic_functions import *
 
 def creation():
-		populus = int(input("Enter the number of Humanoids you wish to create: "))
+	populus = int(input("Enter the number of Humanoids you wish to create: "))
 
-		limit = list(range(0,populus))
-		creations = []
+	limit = list(range(0,populus))
+	creations = []
 
-		for x in limit:
-			name = give_me_name()
-			race = give_me_race()
-			sex = give_me_sex()
-			profession = give_me_profession()
-			powers = give_me_powers()
-			x = Humanoids(name,race,sex,profession,powers)
-			creations.append(x)
-		return creations
+	for x in limit:
+		name = give_me_name()
+		race = give_me_race()
+		sex = give_me_sex()
+		profession = give_me_profession()
+		powers = give_me_powers()
+		x = Humanoids(name,race,sex,profession,powers)
+		creations.append(x)
+	return creations
 
 def know_thy_creation(creation_list):
 	index = list(range(0,len(creation_list)))
